cbebaf5f287f94e02f7112615cee9e2a9a081f7a
dani <danifromecuador@gmail.com>
2023-11-18 18:43:51 -0500

    Write tests for User and Post models following TDD

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'validates presence of required fields' do
+      should validate_presence_of(:user_id)
+      should validate_presence_of(:title)
+      should validate_presence_of(:content)
+      should validate_presence_of(:published)
+    end
+
+    it 'validates relations' do
+      should belong_to(:user)
+    end
+  end
 end
