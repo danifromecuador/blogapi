b243e27f2d0d0cab8108b122ab5490ad689a86c6
dani <danifromecuador@gmail.com>
2023-11-20 19:30:22 -0500

    Create routes and controllers for User create, update, and destroy methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,43 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: @users
+    # GET /users
+    @users = User.all.order(:updated_at).reverse_order
+    render json: @users, status: :ok
   end
 
   def show
+    # GET /users/:id
     @user = User.find_by_id(params[:id])
-    render json: @user
+    render json: @user, status: :ok
+  end
+
+  def create
+    # POST /users
+    User.create(create_params)
+    render json: 'User created', status: :created
+  end
+
+  def update
+    # PATCH /users/:id
+    @user = User.find_by_id(params[:id])
+    @user.update(update_params)
+    render json: 'User updated', status: :ok
+  end
+
+  def destroy
+    # DELETE /users/:id
+    @user = User.find_by_id(params[:id])
+    @user.destroy
+    render json: 'User deleted', status: :ok
+  end
+
+  private
+
+  def create_params
+    params.permit(:name, :email, :auth_token)
+  end
+
+  def update_params
+    params.permit(:name, :email)
   end
 end
